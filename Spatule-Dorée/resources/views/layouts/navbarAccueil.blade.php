79136ebf0611044c2b939c2b9d15d0afd8d26592
Dryss <drysscark@gmail.com>
2023-05-04 10:01:43 +0200

    add Spatule Doree laravel

---
+++ Spatule-Dorée/resources/views/layouts/navbarAccueil.blade.php
@@ -0,0 +1,36 @@
+<nav class="cc-navbar navbar navbar-expand-lg navbar-dark position-fixed w-100">
+    <div class="container-fluid">
+        <a class="navbar-brand text-uppercase mx-4 py-3 fw-bolder" href="">Spatule Dorée</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item pe-4">
+                    <a class="nav-link" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item pe-4">
+                    <a class="nav-link" href="#">Découvrir</a>
+                </li>
+                <li class="nav-item pe-4">
+                    <a class="nav-link" href="#">Les classiques</a>
+                </li>
+                
+                <li class="nav-item pe-4">
+                    <a class="nav-link" href="#">Inscription</a>
+                </li>
+                <li class="nav-item pe-4">
+                    <a class="nav-link" href="#">Se connecter</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
