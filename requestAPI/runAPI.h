bb881b1219c1d62b2b9a940305013ade5428cab3
Sheukn <steven.chouk@gmail.com>
2023-05-19 20:37:31 +0200

    Adding Parser

--- requestAPI/runAPI.h
+++ requestAPI/runAPI.h
@@ -1,9 +1,12 @@
+FILE *outputFile;
+
 size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
-    printf("%.*s\n", (int)(size * nmemb), ptr);
-    return size * nmemb;
+    size_t written = fwrite(ptr, size, nmemb, outputFile);
+    return written;
 }
 
-void runApi(char *url){
+
+void runApi(char *url, char *filename) {
     CURL *curl;
     CURLcode res;
     
@@ -23,16 +26,24 @@ void runApi(char *url){
         // Set the callback function to handle the response
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 
+        // Open the output file for writing
+        outputFile = fopen(filename, "wb");
+        if (outputFile == NULL) {
+            fprintf(stderr, "Failed to open file: %s\n", filename);
+            return;
+        }
+
         // Perform the request
         res = curl_easy_perform(curl);
         if (res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         }
 
-        
-
         // Cleanup
         curl_slist_free_all(headers);
         curl_easy_cleanup(curl);
+
+        // Close the output file
+        fclose(outputFile);
     }
 }
