dac93fcf665e67bac5d2fab26bb426e495866c0c
Dryss <drysscark@gmail.com>
2023-05-21 23:34:00 +0200

    Gestion register

--- Spatule-Dorée/app/Models/User.php
+++ Spatule-Dorée/app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
@@ -15,18 +17,23 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'address',
+        'postal_code',
+        'city',
+        'country',
+        'phone',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -36,9 +43,26 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function create(array $data)
+    {
+        $user = new static;
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->address = $data['address'];
+        $user->postal_code = $data['postal_code'];
+        $user->city = $data['city'];
+        $user->country = $data['country'];
+        $user->phone = $data['phone'];
+        $user->password = Hash::make($data['password']);
+
+        $user->save();
+
+        return $user;
+    }
 }
