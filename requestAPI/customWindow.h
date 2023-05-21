bd9440c36307b9025205361e13c05fda6cf96cab
Sheukn <steven.chouk@gmail.com>
2023-05-21 22:43:20 +0200

    Display results in window

--- requestAPI/customWindow.h
+++ requestAPI/customWindow.h
@@ -2,7 +2,7 @@ void runCustom(GtkWidget *button, gpointer user_data) {
     GtkWidget *window = GTK_WIDGET(user_data);
     GtkWidget *form = gtk_bin_get_child(GTK_BIN(window));
     GtkWidget *urlEntry = gtk_grid_get_child_at(GTK_GRID(form), 1, 0);
-    GtkWidget *methodEntry = gtk_grid_get_child_at(GTK_GRID(form), 1, 1);
+    // GtkWidget *methodEntry = gtk_grid_get_child_at(GTK_GRID(form), 1, 1);
     GtkWidget *apiKeyEntry = gtk_grid_get_child_at(GTK_GRID(form), 1, 2);
     GtkWidget *parameter1Name = gtk_grid_get_child_at(GTK_GRID(form), 1, 4);
     GtkWidget *parameter1Value = gtk_grid_get_child_at(GTK_GRID(form), 2, 4);
@@ -10,9 +10,11 @@ void runCustom(GtkWidget *button, gpointer user_data) {
     GtkWidget *parameter2Value = gtk_grid_get_child_at(GTK_GRID(form), 2, 5);
     GtkWidget *parameter3Name = gtk_grid_get_child_at(GTK_GRID(form), 1, 6);
     GtkWidget *parameter3Value = gtk_grid_get_child_at(GTK_GRID(form), 2, 6);
+    GtkWidget *scrolledView = gtk_grid_get_child_at(GTK_GRID(form), 1, 8);
+    GtkWidget *textView = gtk_bin_get_child(GTK_BIN(scrolledView));
 
     const char *url = gtk_entry_get_text(GTK_ENTRY(urlEntry));
-    const char *method = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(methodEntry));
+    // const char *method = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(methodEntry));
     const char *apiKey = gtk_entry_get_text(GTK_ENTRY(apiKeyEntry));
     const char *parameter1 = gtk_entry_get_text(GTK_ENTRY(parameter1Name));
     const char *parameter1Val = gtk_entry_get_text(GTK_ENTRY(parameter1Value));
@@ -48,18 +50,49 @@ void runCustom(GtkWidget *button, gpointer user_data) {
             strcat(fullUrl, parameter3Val);
         }
 
-        printf("%s\n", fullUrl);
+        char filename[] = "output.txt";
+        runApi(fullUrl, filename);
+        
+        formatToJson(filename, "outputJson.txt");
+
+
+        // Load the file into the text view
+        GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView));
+        gtk_text_buffer_set_text(buffer, "", -1);
+
+        FILE *fp;
+        char line[1000];
+        fp = fopen("outputJson.txt", "r");
+        if (fp == NULL) {
+            printf("Error opening file\n");
+            exit(1);
+        }
+        while (fgets(line, sizeof(line), fp)) {
+            if (g_utf8_validate(line, -1, NULL)) {
+                    gtk_text_buffer_insert_at_cursor(buffer, line, -1);
+                }
+            else {
+                gtk_text_buffer_insert_at_cursor(buffer, "{\n", -1);
+            }
+        }
+        fclose(fp);
+        
+
+        
+        // Free memory and delete files
+        remove("outputJson.txt");
+        remove("output.txt");
+        free(fullUrl);
+
     }
-
 }
-
 
 
 void startCustomWindow(GtkWidget *button, gpointer user_data){
     
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Custom");
-    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
+    gtk_window_set_default_size(GTK_WINDOW(window), 1000, 700);
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
     GtkWidget *form = gtk_grid_new();
@@ -69,32 +102,40 @@ void startCustomWindow(GtkWidget *button, gpointer user_data){
     gtk_grid_attach(GTK_GRID(form), url, 0, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(form), urlEntry, 1, 0, 1, 1);
 
-    GtkWidget *method = gtk_label_new("Method :");
-    GtkWidget *methodEntry = gtk_combo_box_text_new();
+    // GtkWidget *method = gtk_label_new("Method :");
+    // GtkWidget *methodEntry = gtk_combo_box_text_new();
 
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "GET");
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "POST");
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "PUT");
-    gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "DELETE");
+    // gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "GET");
+    // gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "POST");
+    // gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "PUT");
+    // gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(methodEntry), NULL, "DELETE");
 
-    gtk_combo_box_set_active(GTK_COMBO_BOX(methodEntry), 0);
+    // gtk_combo_box_set_active(GTK_COMBO_BOX(methodEntry), 0);
 
-    gtk_grid_attach(GTK_GRID(form), method, 0, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(form), methodEntry, 1, 1, 1, 1);
+    // gtk_grid_attach(GTK_GRID(form), method, 0, 1, 1, 1);
+    // gtk_grid_attach(GTK_GRID(form), methodEntry, 1, 1, 1, 1);
 
     GtkWidget *apiKey = gtk_label_new("API Key :");
     GtkWidget *apiKeyEntry = gtk_entry_new();
     gtk_grid_attach(GTK_GRID(form), apiKey, 0, 2, 1, 1);
     gtk_grid_attach(GTK_GRID(form), apiKeyEntry, 1, 2, 1, 1);
 
+
     GtkWidget *parameters = gtk_label_new("Parameters :");
     gtk_grid_attach(GTK_GRID(form), parameters, 0, 3, 1, 1);
+    gtk_widget_set_margin_top(parameters, 10);
+    gtk_widget_set_margin_bottom(parameters, 10);
 
     GtkWidget *name = gtk_label_new("Name :");
     gtk_grid_attach(GTK_GRID(form), name, 1, 3, 1, 1);
+    gtk_widget_set_margin_top(name, 10);
+    gtk_widget_set_margin_bottom(name, 10);
 
     GtkWidget *value = gtk_label_new("Value :");
     gtk_grid_attach(GTK_GRID(form), value, 2, 3, 1, 1);
+    gtk_widget_set_margin_top(value, 10);
+    gtk_widget_set_margin_bottom(value, 10);
+    
 
     
     GtkWidget *parameter1 = gtk_label_new("Parameter 1 :");
@@ -122,6 +163,17 @@ void startCustomWindow(GtkWidget *button, gpointer user_data){
     GtkWidget *submitButton = gtk_button_new_with_label("Submit");
     g_signal_connect(submitButton, "clicked", G_CALLBACK(runCustom), window);
     gtk_grid_attach(GTK_GRID(form), submitButton, 1, 7, 2, 1);
+
+
+    GtkWidget *textView = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(textView), FALSE);
+
+
+    GtkWidget *scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+    gtk_container_add(GTK_CONTAINER(scrolledWindow), textView);
+    gtk_scrolled_window_set_min_content_height(GTK_SCROLLED_WINDOW(scrolledWindow), 300);
+    gtk_scrolled_window_set_min_content_width(GTK_SCROLLED_WINDOW(scrolledWindow), 800);
+    gtk_grid_attach(GTK_GRID(form), scrolledWindow, 1, 8, 2, 1);
 
 
     gtk_widget_set_halign(form, GTK_ALIGN_CENTER);
