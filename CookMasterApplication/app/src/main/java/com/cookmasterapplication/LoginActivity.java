ac2e2222030fb691c64cd70b998a44f84cb0c36b
Sheukn <steven.chouk@gmail.com>
2023-07-09 16:40:16 +0200

    Change LoginActivity

--- CookMasterApplication/app/src/main/java/com/cookmasterapplication/LoginActivity.java
+++ CookMasterApplication/app/src/main/java/com/cookmasterapplication/LoginActivity.java
@@ -10,6 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -44,17 +54,54 @@ public class LoginActivity extends AppCompatActivity {
                     if (password.getText().toString().isEmpty())
                         password.setError("Veuillez entrer un mot de passe");
 
-                } else {
-                    intent.putExtra("id", identifiant.getText().toString());
-                    intent.putExtra("password", password.getText().toString());
+                }
+                else {
+                    RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                        String url = "https://spatuledoree.fr/api/users";
+                    // Create body request
+                    JSONObject jsonBody = new JSONObject();
+                    String requestBody;
+                    try {
+                        jsonBody.put("email", identifiant.getText().toString());
+                        jsonBody.put("password", password.getText().toString());
+                        requestBody = jsonBody.toString();
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                if (response.equals("false")) {
+                                    Toast.makeText(LoginActivity.this, "Identifiant ou mot de passe incorrect", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    JSONObject jsonObject = new JSONObject(response);
+//                                    edit.putString("token", jsonObject.getString("token"));
+//                                    edit.apply();
+//                                    startActivity(intent);
+//                                    finish();
+                                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, error -> {
+                        Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }) {
+                        @Override
+                        public String getBodyContentType() {
+                            return "application/json; charset=utf-8";
+                        }
 
-                    edit.putString("id", identifiant.getText().toString());
-                    edit.putString("password", password.getText().toString());
-                    edit.apply();
-
+                        @Override
+                        public byte[] getBody() {
+                            return requestBody.getBytes();
+                        }
+                    };
+//                    queue.add(stringRequest);
                     startActivity(intent);
 
-                    finish();
                 }
             }
         });
